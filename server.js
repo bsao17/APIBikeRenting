fb80100cc4674d330ba43ca1a8015ff775caeed1
bsao17 <declic62@gmail.com>
2021-09-29 21:17:36 +0200

    change port for heroku

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const data = require('./data.json')
 
 app.use(cors())
 app.get('/etape', (req, res) => res.status(200).json(data))
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('listening on port 8000')
     })
