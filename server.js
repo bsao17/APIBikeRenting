fdfaaab79dde7b95077d337fcc950c23fdcb6708
bsao17 <declic62@gmail.com>
2021-09-29 19:45:23 +0200

    init APIBikeRenting server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const data = require('./data.json')
+
+app.use(cors())
+app.get('/etape', (req, res) => res.status(200).json(data))
+app.listen(8000, () => {
+    console.log('listening on port 8000')
+    })
